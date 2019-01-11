b2336659a7f4f73c687be87b988e42b60256deae
Lijitha <43061766+lijithabnt@users.noreply.github.com>
2019-01-11 12:54:03 +0530

    Create conversions_units.scala

---
+++ conversions_units.scala
@@ -0,0 +1,17 @@
+//Function to convert temperature from Fahrenheit to Celsius
+val farenToCelsius = (faren:Double) => (faren-32)*5/9
+farenToCelsius(98.6)
+
+//Function to convert miles from km
+val milesToKilometer = (miles:Double) => miles/0.62137
+milesToKilometer(10)
+
+
+//Function to Seconds to years
+val secondsToYears = (seconds:Double) => seconds/31536000
+secondsToYears(234445555)
+
+
+//convert from AU (Astronomical Units) to miles
+val auToMiles = (au:Double) => au/9.296e+7
+auToMiles(34)
