3f662f45695c5226410c0094b151dc7f51abf273
Lijitha <43061766+lijithabnt@users.noreply.github.com>
2019-01-17 00:44:22 +0530

    Create string_Excercise.scala

---
+++ string_Excercise.scala
@@ -0,0 +1,20 @@
+//Given a String from readLine.
+val name:String = scala.io.StdIn.readLine("Name : ")
+//(a) Get a version of the String with all the spaces removed.
+val name1:String = "ABBAVARAM LIJI THA  R E D DY  "
+val spacetrim = (x:String) => (x.replaceAll("\\s",""))
+spacetrim(name1)
+//or
+name1.filterNot((x:Char) => (x.isWhitespace))
+name1.filter((x:Char) => !(x.isWhitespace))
+//(b) Get a version of the String with all vowels removed.
+val vowels = Set('a','e','i','o','u','A','E','I','O','U')
+name1.filter((x:Char) => !(vowels.contains(x)))
+//c) Split off the first word of the String. Write code that gives you a (String, String)
+// where the first element is everything up to the first space in str and the second one is everything else.
+name1.split(" ",2)
+//(d) Convert to a sequence that has the integer values of each of the characters in str.
+val new_str = name1.toIndexedSeq
+//(e) Write code that will take a sequence of Int, like the one you just made, and give you back a String. 
+// (Note that if you get a sequence of Char you can use mkString to get a simple Stringfrom it.)
+val final_str = new_str.mkString.filter((x:Char) => !x.isWhitespace)
