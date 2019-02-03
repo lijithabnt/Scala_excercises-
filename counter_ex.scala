5d5f786fead5c039f382b1d108000f7063fed137
Lijitha <43061766+lijithabnt@users.noreply.github.com>
2019-02-03 16:27:35 +0530

    Create counter_ex.scala

---
+++ counter_ex.scala
@@ -0,0 +1,32 @@
+package lectures.part1Basics
+
+class counter(var c:Int) {
+  def increment(incrtimes: Int): Int = {
+    for (i <- 1 to incrtimes) {
+      c = c + 1
+    }
+    c
+  }
+
+  def decrement(devcal:Int):Int = {
+    c=c-devcal
+    c
+  }
+
+  def increment(incrtimes: Int, incrval: Int) = {
+    for (i <- 1 to incrtimes) {
+      c = c + incrval
+    }
+    c
+  }
+}
+
+object newobj extends  App {
+
+  val ex = new counter(9)
+  println(ex.increment(3))
+  println(ex.decrement(2))
+  println(ex.increment(2, 3))
+  println(ex.c)
+}
+/*
